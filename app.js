186d78a4c8f31ab0498867075197a7635bb46640
Gabrielius <gdobrovolskis97@gmail.com>
2022-03-15 20:08:41 +0000

    changed port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.get("/", (req, res) => {
   console.log("main page");
   res.send("Welcome to the home page");
 });
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
   console.log("Server running on port 3000");
 });
